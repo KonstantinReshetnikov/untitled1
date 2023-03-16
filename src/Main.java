56d7b63c2831b6b378549bc1b771d6d2c8972926
Reshetnikov Konstantin <kostya.reshetnikov.2017@bk.ru>
2023-03-16 20:07:12 +0600

    mycommit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ваше время: ");
         System.out.println("Нынешнее время...");
         System.out.println("Время бла бла бла");
+        System.out.println("Пишу пишу пишу");
     }
 
 }
